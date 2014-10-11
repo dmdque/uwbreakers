4a85185fb0dd1a099d412d225dd5d2671c17b02d
Daniel Que <dmdque@gmail.com>
2014-10-10 23:46:20 -0400

    played around with masonry

--- www/js/custom.js
+++ www/js/custom.js
@@ -1,7 +1,7 @@
 var container = document.querySelector('#container');
 console.log('container', container);
 var msnry = new Masonry( container, {
-  columnWidth: 200,
-  //gutter: 10,
+  //columnWidth: 200,
+  gutter: 10,
   itemSelector: '.item'
 });
