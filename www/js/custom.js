16ede19f865913ade2a8fc59d499670c7fbd9eee
Lily Jiang <itslilyjiang@gmail.com>
2014-10-13 17:10:13 -0400

    fixed headers

--- www/js/custom.js
+++ www/js/custom.js
@@ -1,14 +1,14 @@
-// or with jQuery
-var $container = $('.container');
-// initialize Masonry after all images have loaded  
-$container.imagesLoaded( function() {
-  $container.masonry({
-    columnWidth: 0,
-    itemSelector: '.item'
-  });
-});
+// // or with jQuery
+// var $container = $('.container');
+// // initialize Masonry after all images have loaded  
+// $container.imagesLoaded( function() {
+//   $container.masonry({
+//     columnWidth: 0,
+//     itemSelector: '.item'
+//   });
+// });
 
-//$('.container').masonry({
-  //columnWidth: 0,
-  //itemSelector: '.item'
-//});
+$('.container').masonry({
+  columnWidth: 0,
+  itemSelector: '.item'
+});
