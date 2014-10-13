7d0f9236a4e8fe991a4031bea6108609bad4caa6
Lily Jiang <itslilyjiang@gmail.com>
2014-10-13 12:29:31 -0400

    added imagesloaded to fix masonry

--- www/js/custom.js
+++ www/js/custom.js
@@ -1,3 +1,10 @@
+// or with jQuery
+var $container = $('#container');
+// initialize Masonry after all images have loaded  
+$container.imagesLoaded( function() {
+  $container.masonry();
+});
+
 $('.container').masonry({
   columnWidth: 0,
   itemSelector: '.item'
