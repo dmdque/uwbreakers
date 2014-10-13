a84bcef0b557238f99383a343b1ef57c2e273822
Daniel Que <dmdque@gmail.com>
2014-10-13 15:24:01 -0400

    fixed js

--- www/js/custom.js
+++ www/js/custom.js
@@ -1,11 +1,14 @@
 // or with jQuery
-var $container = $('#container');
+var $container = $('.container');
 // initialize Masonry after all images have loaded  
 $container.imagesLoaded( function() {
-  $container.masonry();
+  $container.masonry({
+    columnWidth: 0,
+    itemSelector: '.item'
+  });
 });
 
-$('.container').masonry({
-  columnWidth: 0,
-  itemSelector: '.item'
-});
+//$('.container').masonry({
+  //columnWidth: 0,
+  //itemSelector: '.item'
+//});
